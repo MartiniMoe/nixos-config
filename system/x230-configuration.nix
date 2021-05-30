69290af8e553ba90809a9ab4c8f210302acc5e4f
MartiniMoe <martini.moe@cheex.de>
2021-05-30 19:54:03 +0200

    Fix hardware configuration to include the one generated by the installer

--- system/x230-configuration.nix
+++ system/x230-configuration.nix
@@ -1,32 +1,10 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    /etc/nixos/hardware-configuration.nix
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/mapper/NIXENC";
-      fsType = "btrfs";
-    };
-
-  boot.initrd.luks.devices."NIXENC".device = "/dev/disk/by-uuid/cbefff03-95af-417e-99ef-13fdf80141a4";
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D313-5306";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/cbe261e7-0c5b-4f14-b052-173bcbadefc3"; }
-    ];
-
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   powerManagement.enable = true;
   
   # Networking
