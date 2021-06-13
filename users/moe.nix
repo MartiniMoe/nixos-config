8a8887bd01f20bd49c0aa108ed9564a26cb51143
MartiniMoe <martini.moe@cheex.de>
2021-06-13 15:12:51 +0200

    Add name for custom keybinding

--- users/moe.nix
+++ users/moe.nix
@@ -60,6 +60,7 @@
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       "binding" = "<Super>Return";
       "command" = "gnome-terminal";
+      "name" = "Terminal";
     };
     "org/gnome/desktop/input-sources" = {
       "current" = "uint32 0";
