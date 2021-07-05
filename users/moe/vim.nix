3bbca8f50a1179828388866c50f54d65f8f6329d
MartiniMoe <martini.moe@cheex.de>
2021-07-05 20:26:42 +0200

    Add basic vim config

--- users/moe/vim.nix
+++ users/moe/vim.nix
@@ -8,12 +8,18 @@
     vimAlias = true;
     vimdiffAlias = true;
     extraConfig = ''
+      syntax on
+      set title
       set clipboard=unnamedplus
       set tabstop=4
       set shiftwidth=4
       set expandtab
       set mouse=r
       set number relativenumber
+      set nocompatible
       '';
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+    ];
   };
 }
